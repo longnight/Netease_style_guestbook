83c6edfbec7bc6901851f15cac1d99052ea37c9a
longnight <longnight@gmail.com>
2014-09-25 17:55:01 +0800

    Moved out local mysql account settings from app
    Moved out local mysql account settings from app to
    your_local_settings.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from tornado import gen
 from tornado.web import asynchronous
 from tornado.options import define, options
 from datetime import datetime
+from your_local_settings import *
 
 class IndexHandler(tornado.web.RequestHandler):
     @property
@@ -75,9 +76,9 @@ def mult_div(com_id, com_dict={}, com_sum=0, coms_list=[]):
         
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="yourdb", help="database name")
-define("mysql_user", default="yourname", help="database user")
-define("mysql_password", default="yourpw", help="database password")
+define("mysql_database", default=db_name, help="database name")
+define("mysql_user", default=db_user, help="database user")
+define("mysql_password", default=db_pw, help="database password")
 
 class Application(tornado.web.Application):
     def __init__(self):
--- app_coroutine.py
+++ app_coroutine.py
@@ -15,9 +15,9 @@ from tornado import gen
 from tornado.web import asynchronous
 from tornado.options import define, options
 from datetime import datetime
-
 from tornado.httpclient import HTTPClient
 from tornado.httpclient import AsyncHTTPClient
+from your_local_settings import *
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -85,9 +85,9 @@ class IndexHandler(tornado.web.RequestHandler):
         
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="yourdb", help="database name")
-define("mysql_user", default="yourname", help="database user")
-define("mysql_password", default="yourpw", help="database password")
+define("mysql_database", default=db_name, help="database name")
+define("mysql_user", default=db_user, help="database user")
+define("mysql_password", default=db_pw, help="database password")
 
 class Application(tornado.web.Application):
     def __init__(self):
